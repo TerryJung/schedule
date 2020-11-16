d0de334903c3a6d14b1d4cd8bdf8fcfd1a2a5183
Eunsol Lee <eunsol@medibloc.org>
2020-11-16 22:53:49 +0900

    minor TODO list added

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import SpoqaHansSansRegular from "./Spoqa Han Sans Regular.ttf";
 // import SpoqaHansSansRegular from "./Weird.ttf";
 
+// TODO: delete every font files except Regular in this folder
 export default createGlobalStyle`
   @font-face {
     font-family: 'Spoqa Han Sans Regular';
